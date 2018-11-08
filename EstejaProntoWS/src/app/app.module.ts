34d63490b93aa85ed30c61e6bfc31fa3f8e763f0
Walther Suguio <suguio@suguio.com.br>
2018-11-08 10:54:35 +0000

    Inicial

--- EstejaProntoWS/src/app/app.module.ts
+++ EstejaProntoWS/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReadProductsComponent } from './products/read-products/read-products.component';
-import { CreateProductsComponent } from './products/create-products/create-products.component';
-import { UpdateProductsComponent } from './products/update-products/update-products.component';
-import { DeleteProductsComponent } from './products/delete-products/delete-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ReadProductsComponent,
-    CreateProductsComponent,
-    UpdateProductsComponent,
-    DeleteProductsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
